e023199a2b427d15ec94084078436b4bacfac36b
JOHN L BUTCHART <fugitivepoet@Mac.lan>
2026-04-19 23:13:33 -0400

    Fix TypeScript params type

--- app/artist/[id]/page.tsx
+++ app/artist/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { supabase } from '@/lib/supabase'
 import ArtistClient from './ArtistClient'
 
-export default async function ArtistPage({ params }) {
+export default async function ArtistPage({ params }: { params: Promise<{ id: string }> }) {
   const id = (await params).id
 
   // Fetch artist
